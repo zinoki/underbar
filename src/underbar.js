07886a3873d2a11ef7701740a57e4e6998a3f21f
Zachary <zinouye@gmail.com>
2017-12-19 02:10:30 -0800

    Finished part 1 of underbar

--- src/underbar.js
+++ src/underbar.js
@@ -128,6 +128,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    _.each(collection, function(value) {
+      results.push(iterator(value));
+    })
+    return results;
   };
 
   /*
@@ -169,6 +174,16 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if (accumulator !== undefined) {
+      var result = accumulator;
+    } else {
+      var result = collection[0];
+      collection = collection.slice(1);
+    }
+    _.each(collection, function(i) {
+      result = iterator(result, i);
+    })
+    return result;
   };
 
   // Determine if the array or object contains a given value (using `===`).
